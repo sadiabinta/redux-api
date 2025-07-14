3087e5c8dd4a22c2888c80e36c7412f9c442bb14
sadiabinta <sadiaa.kabir@gmail.com>
2025-07-14 11:38:35 +0600

    hfaj

--- src/redux/api/bookCreateAPI.ts
+++ src/redux/api/bookCreateAPI.ts
@@ -4,8 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const booksApi = createApi({
   reducerPath: "bookApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
-    // || "http://localhost:5000/api",
+    baseUrl: `${import.meta.env.VITE_API_BASE_URL}/api`,
   }),
   tagTypes: ["Books"],
   endpoints: (builder) => ({
--- src/redux/api/borrowBookAPI.ts
+++ src/redux/api/borrowBookAPI.ts
@@ -4,8 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const borrowBookAPI = createApi({
   reducerPath: "borrowBookAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
-    // || "http://localhost:5000/api",
+    baseUrl: `${import.meta.env.VITE_API_BASE_URL}/api`,
   }),
   tagTypes: ["borrowedBooks"],
   endpoints: (builder) => ({
